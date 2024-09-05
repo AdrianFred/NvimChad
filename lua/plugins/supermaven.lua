5788584494f284a3dae08db8a11f4148a7f86e73
AdrianFred <adrian989@me.com>
2024-09-05 23:58:24 +0200

    Batman

---
+++ lua/plugins/supermaven.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    lazy = false,
+    opts = {
+    keymaps = {
+      accept_suggestion = "<Tab>",
+      clear_suggestion = "<C-]",
+      accept_word = "<C-y>",
+    },
+    -- color = {
+    --   suggestion_color = "#ffffff",
+    --   cterm = 244,
+    -- },
+    disable_keymaps = false,
+    log_level = "info",
+    dsable_inline_completion = false,
+    },
+  },
+}
