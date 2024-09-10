56d2dc0c0114c8bedde68cfedca37c76ca0c6fe1
AdrianFred <adrian989@me.com>
2024-09-10 13:09:19 +0200

    test

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,20 +1,20 @@
 return {
-  -- {
-  --   "supermaven-inc/supermaven-nvim",
-  --   event = { "BufReadPre", "BufNewFile" },
-  --   opts = {
-  --     keymaps = {
-  --       accept_suggestion = "<Tab>",
-  --       clear_suggestion = "<C-]",
-  --       accept_word = "<C-y>",
-  --     },
-  --     -- color = {
-  --     --   suggestion_color = "#ffffff",
-  --     --   cterm = 244,
-  --     -- },
-  --     disable_keymaps = false,
-  --     log_level = "info",
-  --     dsable_inline_completion = false,
-  --   },
-  -- },
+  {
+    "supermaven-inc/supermaven-nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      keymaps = {
+        accept_suggestion = "<Tab>",
+        clear_suggestion = "<C-]",
+        accept_word = "<C-y>",
+      },
+      -- color = {
+      --   suggestion_color = "#ffffff",
+      --   cterm = 244,
+      -- },
+      disable_keymaps = false,
+      log_level = "info",
+      dsable_inline_completion = false,
+    },
+  },
 }
